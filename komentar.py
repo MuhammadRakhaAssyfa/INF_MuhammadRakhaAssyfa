bcbd2ebfa9f89fb624f8d7da8c390d4bf87ee7c7
Muhammad Rakha Assyfa <muhamadraka436@gmail.com>
2025-04-28 11:06:17 +0700

    Komentar

--- komentar.py
+++ komentar.py
@@ -1,1 +1,15 @@
-#ini komentar ygy
+#ini cuma bisa satu baris
+
+""""
+ini komentar
+buat lebih dari 1 baris
+"""
+print("suka tidur, makan, nonton, mandi.")
+"""
+print("suka tidur, makan, nonton, mandi.")
+"""
+#nama
+print("Rakha")
+
+#tanggal buat
+print("28-04-2025")

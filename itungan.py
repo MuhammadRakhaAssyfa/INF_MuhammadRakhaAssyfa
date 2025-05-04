5a82ac661b2fd8f66308e598de6a005779062118
Muhammad Rakha Assyfa <muhamadraka436@gmail.com>
2025-05-04 23:27:11 +0700

    Komentar

---
+++ itungan.py
@@ -0,0 +1,25 @@
+print("Penjumlahan dua bilangan")
+a1 = int(input("Masukkan bilangan pertama: "))
+a2 = int(input("Masukkan bilangan kedua: "))
+print("Hasil penjumlahan: ", a1 + a2)
+
+print()
+
+print("Pengurangan dua bilangan")
+a1 = int(input("Masukkan bilangan pertama: "))
+a2 = int(input("Masukkan bilangan kedua: "))
+print("Hasil pengurangan: ", a1 - a2)
+
+print()
+
+print("Perkalian dua bilangan")
+a1 = int(input("Masukkan bilangan pertama: "))
+a2 = int(input("Masukkan bilangan kedua: "))
+print("Hasil perkalian: ", a1 * a2)
+
+print()
+
+print("Pembagian dua bilangan")
+a1 = int(input("Masukkan bilangan pertama: "))
+a2 = int(input("Masukkan bilangan kedua: "))
+print("Hasil pembagian: ", a1 / a2)
---
+++ variabel.py
@@ -0,0 +1,14 @@
+nama = "rakha"
+print(nama)
+
+umur = 20
+print(umur)
+
+namadepan = "Muhammad"
+namabelakang = "Rakha"
+print(namadepan + " " + namabelakang)
+
+namaDepan = input("Masukkan nama depan anda: ")
+namaBelakang = input("Masukkan nama belakang anda: ")
+umur = input("Masukkan umur anda: ")
+print("Nama saya " + namaDepan + " " + namaBelakang + " dan umur saya " + umur + " tahun.")

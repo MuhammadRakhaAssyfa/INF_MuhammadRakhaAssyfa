e64711b8dfcd4578d49fb9023e7bb329a37fe284
Muhammad Rakha Assyfa <muhamadraka436@gmail.com>
2025-05-05 10:58:37 +0700

    Tipe Data

--- td.py
+++ td.py
@@ -3,6 +3,6 @@ umur = int(input("Berapa umurmu? ")) #integer
 tinggi = float(input("Berapa tinggi badanmu (meter)? ")) #float
                      
 print("\n--- DATA DIRI ---")
-print("Nama saya   :", nama)
-print("Umur saya   :", umur, "tahun")
-print("Tinggi saya :", tinggi, "meter")
+print("Nama saya    :", nama)
+print("Umur saya    :", umur, "tahun")
+print("Tinggi saya  :", tinggi, "meter")

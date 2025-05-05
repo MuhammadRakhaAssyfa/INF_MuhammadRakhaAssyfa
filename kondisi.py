1d135572141b049f0f159f4f7e6127e5c101dc0c
Muhammad Rakha Assyfa <muhamadraka436@gmail.com>
2025-05-05 10:19:29 +0700

    Update String

---
+++ kondisi.py
@@ -0,0 +1,19 @@
+#Kondisi If Else
+nilai = int(input("Masukkan nilai [1-10]: "))
+if (nilai > 5):
+    print("Lulus")
+else:
+    print("Tidak Lulus")
+
+#Kondisi Elif
+nilai = int(input("Masukkan nilai [1-100]: "))
+if (nilai >= 80):
+    print("Nilai Anda : A")
+elif (nilai >= 70):
+    print("Nilai Anda : B")
+elif (nilai >= 60):
+    print("Nilai Anda : C")
+elif (nilai >= 50):
+    print("Nilai Anda : D")
+else:
+    print("Nilai Anda : E")
---
+++ us.py
@@ -0,0 +1,3 @@
+pesan1 = "Hallo semua"
+print(pesan1)
+print(pesan1 + " Nama saya Rakha")

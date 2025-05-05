f24fd057d9f0f852cff519284ebe638ddecadc26
Muhammad Rakha Assyfa <muhamadraka436@gmail.com>
2025-05-05 10:56:41 +0700

    Operator

--- itungan.py
+++ itungan.py
@@ -3,21 +3,16 @@ a1 = int(input("Masukkan bilangan pertama: "))
 a2 = int(input("Masukkan bilangan kedua: "))
 print("Hasil penjumlahan: ", a1 + a2)
 
-print()
-
 print("Pengurangan dua bilangan")
 a1 = int(input("Masukkan bilangan pertama: "))
 a2 = int(input("Masukkan bilangan kedua: "))
 print("Hasil pengurangan: ", a1 - a2)
-
-print()
 
 print("Perkalian dua bilangan")
 a1 = int(input("Masukkan bilangan pertama: "))
 a2 = int(input("Masukkan bilangan kedua: "))
 print("Hasil perkalian: ", a1 * a2)
 
-print()
 
 print("Pembagian dua bilangan")
 a1 = int(input("Masukkan bilangan pertama: "))

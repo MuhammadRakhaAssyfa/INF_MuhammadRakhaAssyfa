22f0a5e8d8e124a3d2de20e01c1433e4d7f3270b
Muhammad Rakha Assyfa <muhamadraka436@gmail.com>
2025-05-08 13:41:27 +0700

    Pengulangan

--- loop.py
+++ loop.py
@@ -3,7 +3,12 @@ i = 1
 while i <= 10:
     print("angka ke-", i)
     i += 1
-
+print()
+i = int(input("Masukkan angka: "))
+while i <= 20:
+    print("angka ke-", i)
+    i += 2
+print()
 #For Loop
 for i in range(1, 11):
     print("angka ke-", i)
